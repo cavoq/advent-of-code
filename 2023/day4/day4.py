d148097c0f53463854a30b87537fa14928c8e6af
cavoq <Davidstromberger99@gmx.de>
2023-12-05 22:08:39 +0100

    Day 4 part 2

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -1,4 +1,5 @@
 
+
 INPUT_FILE = "input.txt"
 
 
@@ -7,6 +8,8 @@ class Card:
         self.id = id
         self.numbers = numbers
         self.winning_numbers = winning_numbers
+        self.won_copies = 1
+        self.won_copies_indexes = []
 
     @classmethod
     def from_string(cls, string: str):
@@ -33,20 +36,47 @@ class Card:
                 score *= multiplier
         return score
 
+    def set_won_copies_indexes(self) -> list[int]:
+        index = 1
+        for number in self.numbers:
+            if number in self.winning_numbers:
+                self.won_copies_indexes.append(self.id + index)
+                index += 1
+        return self.won_copies_indexes
 
-def read_data() -> list[Card]:
+    def get_won_copies_indexes(self) -> list[int]:
+        return self.won_copies_indexes
+
+
+def read_data() -> dict[int, Card]:
+    cards = {}
     with open(INPUT_FILE) as f:
         lines = f.read().splitlines()
-    return [Card.from_string(line) for line in lines]
+        for line in lines:
+            card = Card.from_string(line)
+            cards[card.id] = card
+    return cards
 
 
-def part1(cards: list[Card]):
+def part1(cards: dict[int, Card]):
     total_score = 0
-    for card in cards:
+    for card in cards.values():
         total_score += card.get_score()
     print("Part 1:", total_score)
 
 
+def part2(cards: dict[int, Card]):
+    for card in cards.values():
+        card.set_won_copies_indexes()
+        indexes = card.get_won_copies_indexes()
+        for index in indexes * card.won_copies:
+            cards[index].won_copies += 1
+
+    total_cards_won = sum(card.won_copies for card in cards.values())
+    print("Part 2:", total_cards_won)
+
+
 if __name__ == "__main__":
     cards = read_data()
-    score = part1(cards)
+    part1(cards)
+    part2(cards)

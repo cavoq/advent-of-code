e29e532854a917d33ceab256a411ecb35bff9d01
David Stromberger <cavoq@proton.me>
2024-12-02 12:14:41 +0100

    Day 2: part 2

--- 2024/day2/day2.go
+++ 2024/day2/day2.go
@@ -9,66 +9,92 @@ import (
 )
 
 type Record struct {
-  Values []int
-  Safe bool
+	Values []int
+	Safe   bool
 }
 
 func readRecords(path string) []Record {
-  file, err := os.Open(path)
-  if err != nil {
-    panic(err)
-  }
-  defer file.Close()
-
-  records := []Record{}
-  scanner := bufio.NewScanner(file)
-
-  for scanner.Scan() {
-    line := scanner.Text()
-    parts := strings.Fields(line) 
-    values := []int{}
-
-    for _, part := range parts {
-      value, err := strconv.Atoi(part)
-      if err != nil {
-        panic(err)
-      }
-      values = append(values, value)
-    }
-
-    records = append(records, Record{Values: values})
-  }
-
-  return records
-}
-
-func reverse(values []int) []int {
-	for i, j := 0, len(values)-1; i < j; i, j = i+1, j-1 {
-		values[i], values[j] = values[j], values[i]
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
 	}
-	return values
+	defer file.Close()
+
+	records := []Record{}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+		values := []int{}
+
+		for _, part := range parts {
+			value, err := strconv.Atoi(part)
+			if err != nil {
+				panic(err)
+			}
+			values = append(values, value)
+		}
+
+		records = append(records, Record{Values: values, Safe: true})
+	}
+
+	return records
 }
 
-func (r *Record) isSafe(minV, maxV int) bool {
-  if r.Values[0] > r.Values[1] {
-    r.Values = reverse(r.Values)
-  }
-  for i := 0; i < len(r.Values) - 1; i++ {  
-    distance := r.Values[i+1] - r.Values[i]
-    if distance < minV || distance > maxV {
-      return false
-    }
-  }
-  return true
+func diff(num1, num2 int) (int, int) {
+	if num1 > num2 {
+		return num1 - num2, -1
+	}
+	return num2 - num1, 1
+}
+
+func isSafe(minV, maxV int, slice []int) bool {
+	direction := 1
+	if slice[0] > slice[1] {
+		direction = -1
+	}
+	for i := 0; i < len(slice)-1; i++ {
+		distance, newDir := diff(slice[i], slice[i+1])
+		if distance < minV || distance > maxV || newDir != direction {
+			return false
+		}
+	}
+	return true
+}
+
+func isSafe2(minV, maxV int, values []int) bool {
+	if isSafe(minV, maxV, values) {
+		return true
+	}
+
+	for i := 0; i < len(values); i++ {
+		slice := make([]int, len(values))
+		copy(slice, values)
+		slice = append(slice[:i], slice[i+1:]...)
+		if isSafe(minV, maxV, slice) {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
-  records := readRecords("input.txt")
-  safeRecords := 0
-  for _, record := range records {
-    if record.isSafe(1, 3) {
-      safeRecords++
-    }
-  }
-  fmt.Println("Part 1:", safeRecords)
+	records := readRecords("input.txt")
+
+	safeRecords := 0
+	for i := 0; i < len(records); i++ {
+		if isSafe(1, 3, records[i].Values) {
+			safeRecords++
+		}
+	}
+	fmt.Println("Part 1:", safeRecords)
+
+	safeRecords = 0
+	for i := 0; i < len(records); i++ {
+		if isSafe2(1, 3, records[i].Values) {
+			safeRecords++
+		}
+	}
+	fmt.Println("Part 2:", safeRecords)
 }
